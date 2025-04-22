21d744bf8d315b5cfd9df6e0bb10f49b9bceabc4
Sai Sashreek D <saisashreekd@gmail.com>
2025-04-22 16:49:34 +0530

    Added New Footer Component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import Footer from "./(Components)/Footer";
 import Header from "./(Components)/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
