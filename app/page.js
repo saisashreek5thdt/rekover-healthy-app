22a59d1ea00e02f144c881747f70462f4d94d72b
Sai Sashreek D <saisashreekd@gmail.com>
2025-04-23 17:37:46 +0530

    Updated with new sections which are steps and doctors team

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
 import Banner from "./(Components)/Home/Banner";
+import Clients from "./(Components)/Home/Clients";
+import DoctorsTeam from "./(Components)/Home/DoctorsTeam";
+import StepsSection from "./(Components)/Home/StepsSection";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <Clients />
+      <StepsSection />
+      <DoctorsTeam />
     </>
   );
 }
