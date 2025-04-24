18f7160d168cd721b7c43ddc959e2dafb121f684
Sai Sashreek D <saisashreekd@gmail.com>
2025-04-24 10:03:33 +0530

    Updated Meta Description

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,13 @@
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Footer from "./(Components)/Footer";
 import Header from "./(Components)/Header";
 import "./globals.css";
 
 export const metadata = {
   title: "ReKover Healthy App",
-  description: "ReKover Healthy Application, where solution meets health.",
+  description:
+    "ReKover Healthy Application, Where Solution Meets Health. Welcome to ReKover—your all-in-one digital health platform designed to seamlessly connect healthcare providers with patients, and promote wellness across both corporate and personal spaces.",
 };
 
 export default function RootLayout({ children }) {
