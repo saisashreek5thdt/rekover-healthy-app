0f4f0cee70efc3837e8922e595997aa3ac9f347c
Sai Sashreek D <saisashreekd@gmail.com>
2025-04-25 16:53:18 +0530

    Added New Components

--- app/page.js
+++ app/page.js
@@ -2,7 +2,9 @@ import Banner from "./(Components)/Home/Banner";
 import Clients from "./(Components)/Home/Clients";
 import DoctorsTeam from "./(Components)/Home/DoctorsTeam";
 import Features from "./(Components)/Home/Features";
+import Pricing from "./(Components)/Home/Pricing";
 import StepsSection from "./(Components)/Home/StepsSection";
+import Testimonials from "./(Components)/Home/Testimonials";
 
 export default function Home() {
   return (
@@ -12,6 +14,8 @@ export default function Home() {
       <Features />
       <StepsSection />
       <DoctorsTeam />
+      <Pricing />
+      <Testimonials />
     </>
   );
 }
